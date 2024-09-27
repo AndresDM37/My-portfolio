75f55862ff8ddc344260c123ab749e3f236f11f1
andresmarchenaporras@gmail.com <Andres37DM>
2024-09-26 23:06:26 -0500

    home

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+export function Navbar() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Skills</a>
+          </li>
+          <li>
+            <a href="#projects">Works</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
