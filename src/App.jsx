ee440b81782336146135e26d252f09dee74893c7
andresmarchenaporras@gmail.com <Andres37DM>
2024-10-02 21:07:52 -0500

    arquitectura

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import "./App.css";
-import { Navbar } from "./components/Navbar";
-import { Home } from "./components/Home";
+import { Page } from "./Page/Landing";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-    </>
+    <main>
+      <Page />
+    </main>
   );
 }
 
