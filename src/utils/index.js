cb2c4f2874b7423c0bf42a8a066a55d01a1cc7dd
andresmarchenaporras@gmail.com <Andres37DM>
2025-01-07 23:12:57 -0500

    StarIcon

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,57 +7,52 @@ export const calculatedSizes = (isSmall, isMobile, isTablet, isDesktop) => {
       : isTablet
       ? 0.065
       : isDesktop
-      ? 0.08 // Escala específica para escritorio grande
-      : 0.07, // Escala predeterminada para otros tamaños
-
+      ? 0.08
+      : 0.1, // Escala para pantallas más grandes
     deskPosition: isSmall
-      ? [0.5, -4.5, 0]
+      ? [0, -3, 0]
       : isMobile
-      ? [0.5, -4.5, 0]
+      ? [0, -4, 0]
       : isTablet
-      ? [0.25, -5.5, 0]
+      ? [0, -5, 0]
       : isDesktop
-      ? [0.5, -10, 0] // Posición ajustada para escritorio
-      : [0.25, -5.5, 0], // Predeterminada
-
+      ? [0, -13, 0]
+      : [0, -7, 0], // Ajuste para pantallas grandes
     cubePosition: isSmall
+      ? [2, -4, 0]
+      : isMobile
+      ? [3, -5, 0]
+      : isTablet
       ? [4, -5, 0]
-      : isMobile
-      ? [5, -5, 0]
-      : isTablet
-      ? [5, -5, 0]
       : isDesktop
-      ? [8, -6, 0] // Ajuste para escritorio
-      : [9, -5.5, 0],
-
+      ? [5, -6, 0]
+      : [6, -7, 0],
     reactLogoPosition: isSmall
-      ? [3, 4, 0]
+      ? [0, 4, 0]
       : isMobile
-      ? [5, 4, 0]
+      ? [0, 4.5, 0]
       : isTablet
-      ? [5, 4, 0]
+      ? [0, 5, 0]
       : isDesktop
-      ? [10, 3, 0] // Posición para escritorio
-      : [12, 3, 0],
-
+      ? [0, 5.5, 0]
+      : [0, 6, 0],
     ringPosition: isSmall
-      ? [-5, 7, 0]
+      ? [-5, 6, 0]
       : isMobile
-      ? [-10, 10, 0]
+      ? [-6, 7, 0]
       : isTablet
-      ? [-12, 10, 0]
+      ? [-8, 8, 0]
       : isDesktop
-      ? [-20, 12, 0] // Posición ajustada
-      : [-24, 10, 0],
-
+      ? [-10, 9, 0]
+      : [-12, 10, 0],
     targetPosition: isSmall
-      ? [-5, -10, -10]
+      ? [-4, -9, -10]
       : isMobile
-      ? [-9, -10, -10]
+      ? [-5, -10, -10]
       : isTablet
-      ? [-11, -7, -10]
+      ? [-6, -11, -10]
       : isDesktop
-      ? [-15, -12, -10] // Ajuste para escritorio
-      : [-13, -13, -10],
+      ? [-7, -12, -10]
+      : [-8, -13, -10],
   };
 };
